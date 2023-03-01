d480a8c723810ebff8484e26371287fb4c3826e8
Templeton Robotics <templeton7190@gmail.com>
2023-02-28 21:44:01 -0800

    final tweaks

--- commands/xboxcontroller_drive.py
+++ commands/xboxcontroller_drive.py
@@ -9,16 +9,16 @@ class XboxcontrollerDrive(commands2.CommandBase):
     
     def execute(self) -> None:
         forward_speed = self.xboxcontroller.getLeftY()
-        if abs(self.xboxcontroller.getLeftY()) < 0.3:
-            if self.xboxcontroller.getLeftY() > 0:
-                forward_speed = constants.SLOW_FIXED_SPEED
-            if self.xboxcontroller.getLeftY() > 0:
-                forward_speed = -constants.SLOW_FIXED_SPEED
-        rotation_speed = self.xboxcontroller.getLeftY()
-        if abs(self.xboxcontroller.getRightX()) < 0.3:
-            if self.xboxcontroller.getRightX() > 0:
-                rotation_speed = constants.SLOW_FIXED_SPEED
-            if self.xboxcontroller.getRightX() > 0:
-                rotation_speed = -constants.SLOW_FIXED_SPEED
+        # if abs(self.xboxcontroller.getLeftY()) < 0.3:
+        #     if self.xboxcontroller.getLeftY() > 0:
+        #         forward_speed = constants.SLOW_FIXED_SPEED
+        #     if self.xboxcontroller.getLeftY() > 0:
+        #         forward_speed = -constants.SLOW_FIXED_SPEED
+        rotation_speed = self.xboxcontroller.getRightX()
+        # if abs(self.xboxcontroller.getRightX()) < 0.3:
+        #     if self.xboxcontroller.getRightX() > 0:
+        #         rotation_speed = constants.SLOW_FIXED_SPEED
+        #     if self.xboxcontroller.getRightX() > 0:
+        #         rotation_speed = -constants.SLOW_FIXED_SPEED
         self.drive_train.move(forward_speed, -rotation_speed)
     
